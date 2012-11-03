51597f852f46df1114f036a59b490446f311ca9d
Danny Coates <dannycoates@gmail.com>
2012-11-03 16:08:23 -0700

    renamed protocol dir to client

--- index.js
+++ index.js
@@ -3,21 +3,20 @@ var os = require('os')
 var async = require('async')
 var inherits = require('util').inherits
 var EventEmitter = require('events').EventEmitter
-var Client = require('./protocol')
-
 var ZooKeeper = require('zookeeper')
-var MessageBuffer = require('./message-buffer')()
-var Topic = require('./topic')(inherits, EventEmitter, MessageBuffer)
 
+var Client = require('./client')
 var Partition = require('./partition')()
 var Owner = require('./owner')(Partition)
+var Consumer = require('./consumer')(async, os, inherits, EventEmitter, Owner)
 var Broker = require('./broker')(inherits, EventEmitter, Client)
 var BrokerPool = require('./broker-pool')(inherits, EventEmitter)
 var Producer = require('./producer')(inherits, EventEmitter, BrokerPool)
-var Consumer = require('./consumer')(async, os, inherits, EventEmitter, Owner)
+var StaticConnector = require('./static-connector')(inherits, EventEmitter, Producer, Consumer, BrokerPool, Broker)
 var ZK = require('./zk')(async, inherits, EventEmitter, ZooKeeper)
 var ZKConnector = require('./zkconnector')(async, inherits, EventEmitter, ZK, Producer, Consumer, BrokerPool, Broker)
-var StaticConnector = require('./static-connector')(inherits, EventEmitter, Producer, Consumer, BrokerPool, Broker)
+var MessageBuffer = require('./message-buffer')()
+var Topic = require('./topic')(inherits, EventEmitter, MessageBuffer)
 var kafka = require('./kafka')(inherits, EventEmitter, Topic, ZKConnector, StaticConnector, Client.compression)
 
 module.exports = kafka
